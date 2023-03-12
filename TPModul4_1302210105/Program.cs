eb7adcc90497c303e57f4c33eb773103da95d1e0
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-03-13 00:46:39 +0700

    menambahkan implemantasi dengan tabel-driven

--- TPModul4_1302210105/Program.cs
+++ TPModul4_1302210105/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class KodePos
+{
+    private static readonly System.Collections.Generic.Dictionary<string, string> kodePosTable = new System.Collections.Generic.Dictionary<string, string>() {
+        { "Batununggal", "40266" },
+        { "A. Kujangsari", "40287" },
+        { "Mengger", "40267" },
+        { "Wates", "40256" },
+        { "Cijaura", "40287" },
+        { "Jatisari", "40286" },
+        { "Margasari", "40286" },
+        { "Sekejati", "40286" },
+        { "Kebonwaru", "40272" },
+        { "Maleer", "40274" },
+        { "Samoja", "40273" }
+    };
+
+    public static string GetKodePos(string kelurahan)
+    {
+        if (kodePosTable.ContainsKey(kelurahan))
+        {
+            return kodePosTable[kelurahan];
+        }
+        return "Kode Pos tidak ditemukan.";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string kodePos = KodePos.GetKodePos("Wates");
+        Console.WriteLine(kodePos);
+
+        kodePos = KodePos.GetKodePos("Samoja");
+        Console.WriteLine(kodePos);
+
+        kodePos = KodePos.GetKodePos("Bandung");
+        Console.WriteLine(kodePos);
+    }
+}
